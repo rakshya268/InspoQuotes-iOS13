42e4ad501fd642a16bff44a20e0b512f039ccf61
Rajiv karki <rajiv.karki@outlook.com>
2022-06-19 11:54:30 -0600

    Init repo

---
+++ InspoQuotes/QuoteTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  QuoteTableViewController.swift
+//  InspoQuotes
+//
+//  Created by Angela Yu on 18/08/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class QuoteTableViewController: UITableViewController {
+   
+    let productID = ""
+    
+    var quotesToShow = [
+        "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
+        "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius",
+        "Everything you’ve ever wanted is on the other side of fear. — George Addair",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
+    ]
+    
+    let premiumQuotes = [
+        "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
+        "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
+        "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
+        "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView:UITableView,numberOfRowsInSection section:Int)->Int {
+        return quotesToShow.count + 1
+    }
+
+    
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        
+        if indexPath.row < quotesToShow.count {
+        cell.textLabel?.text = quotesToShow[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        } else {
+            cell.textLabel?.text = "Get More Qotes"
+            cell.textLabel?.textColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            cell.accessoryType = .disclosureIndicator
+        }
+            
+        return cell
+    }
+    
+    // Mark :-Table view delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == quotesToShow.count {
+            buyPremiumQotes ()
+        }
+        tableView.deselectRow(at:indexPath,animated:true)
+        
+    }
+   
+    //Mark:- In app purchase Methods
+    
+    func buyPremiumQotes(){
+        if SKPaymentQueue.canMakePayments() {
+            //Can make payements
+          let paymentRequest = SKMutablePayment()
+            paymentRequest.productIdentifier = productID
+            SKPaymentQueue.default().add(payementRequest)
+            
+        }else {
+            //can't make payements
+            print("user cant make payments")
+        }
+        
+    }
+   
+
+    @IBAction func restorePressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+
+}
